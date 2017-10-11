70bf0fea89d2349fc7022f5246037b9ba6d69687
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-10-12 01:10:05 +0300

    PARTY-INVITES project was done.

---
+++ PARTY-INVITES/PARTY-INVITES/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PARTY_INVITES.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Pleas enter your name")]
+        public string Name       { get; set; }
+
+        [RegularExpression(".+\\@.+\\..+", 
+            ErrorMessage = "Pleas enter your email address")]
+        public string Email      { get; set; }
+
+        [Required(ErrorMessage = "Pleas enter your phone number")]
+        public string Phone      { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool?  WillAttend { get; set; }
+    }
+}
