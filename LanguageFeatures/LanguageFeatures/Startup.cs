fec5bda6886b6a203fc6f25ebb706b2cd1ff27c1
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-10-12 12:24:07 +0300

    'Startup.cs' file was amended

--- LanguageFeatures/LanguageFeatures/Startup.cs
+++ LanguageFeatures/LanguageFeatures/Startup.cs
@@ -15,7 +15,7 @@ namespace LanguageFeatures
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +30,7 @@ namespace LanguageFeatures
             {
                 await context.Response.WriteAsync("Hello World!");
             });
-            app.UseMvcWithDefaultRoute();
+            app.UseMvcWithDefaultRoute(); 
         }
     }
 }
