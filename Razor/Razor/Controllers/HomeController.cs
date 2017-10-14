ea320a5f1686416bc6fdfd2f70ca83bbf0a9cd71
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-10-14 11:09:45 +0300

    Razor project was done.

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product product = new Product
+            {
+                ProductId   = 1,
+                Name        = "Kayak",
+                Category    = "Watersports",
+                Description = "А boat for one person",
+                Price       = 275M,
+            };
+            return View(product);
+        }
+    }
+}
