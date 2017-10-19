1bb856b391266fdbb68d94d5c38ae679ccfed192
Eugene Miroshnichenko <codelookbook@gmail.com>
2017-10-19 15:35:24 +0300

    SportsStore project => Product Controller class was created List() action method.

--- SportsStore/SportsStore/Controllers/ProductController.cs
+++ SportsStore/SportsStore/Controllers/ProductController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        #region PROPERTIES
+        // PRIVATE
+        private IProductRepository repository;
+        #endregion
+
+
+        #region CONSTRUCTORS
+        public ProductController(IProductRepository repository)
         {
-            return View();
+            this.repository = repository;
         }
+        #endregion
+
+        #region ACTIONS
+        public IActionResult List() => View(this.repository.Products);
+        #endregion
     }
 }
