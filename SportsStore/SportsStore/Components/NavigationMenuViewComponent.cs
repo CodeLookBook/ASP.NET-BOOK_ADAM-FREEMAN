7241174cae27023e84aa548f540688e3c529d85d
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-15 19:02:12 +0300

    SelectedCategory info was added to Invoke method of the NavigationMenuViewComponent class and it can be used un the view now.

--- SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -19,6 +19,8 @@ namespace SportsStore.Components
 
         public IViewComponentResult Invoke()
         {
+            ViewBag.SelectedCategory = this.RouteData?.Values["category"];
+
             return View(this.repository
                 .Products
                 .Select(x => x.Category)
