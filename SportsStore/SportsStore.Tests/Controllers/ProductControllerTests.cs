401e5f766e81af0f775291d60488d5a76986ab06
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-15 20:03:01 +0300

    Test for specific product categories coints was created.

--- SportsStore/SportsStore.Tests/Controllers/ProductControllerTests.cs
+++ SportsStore/SportsStore.Tests/Controllers/ProductControllerTests.cs
@@ -27,7 +27,7 @@ namespace SportsStore.Tests.Controllers
                 new Product { ProductID=6, Name="P6", Category="C3" },
             }.AsQueryable());
 
-            var controller = new ProductController(mock.Object) { PageSize = 2 };
+            var controller = new ProductController(mock.Object) { PageSize = 3};
 
             // Act
             var result   = controller.List("C2")     as ViewResult;
@@ -48,6 +48,43 @@ namespace SportsStore.Tests.Controllers
             Assert.Equal(2   , model.PagingInfo.ItemsPerPage        ); 
             Assert.Equal(1   , model.PagingInfo.CurrentPage         );
             Assert.Equal(2   , model.PagingInfo.TotalItems          );
+        }
+
+        [Fact]
+        public void List_CanSendSpecificProductCategoryCount()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+            }).AsQueryable<Product>());
+
+            ProductController controller = new ProductController(mock.Object);
+
+            controller.PageSize = 3;
+
+            ProductsListViewModel GetModel(IActionResult cntrl) => 
+                (cntrl as ViewResult)
+                ?.ViewData
+                ?.Model as ProductsListViewModel;
+
+            // Action
+            int? res1 = GetModel(controller.List("Cat1"))?.PagingInfo.TotalItems;
+            int? res2 = GetModel(controller.List("Cat2"))?.PagingInfo.TotalItems;
+            int? res3 = GetModel(controller.List("Cat3"))?.PagingInfo.TotalItems;
+            int? resAll = GetModel(controller.List(null))?.PagingInfo.TotalItems;
+
+            // Assert    
+            Assert.Equal(2, res1);
+            Assert.Equal(2, res2);
+            Assert.Equal(1, res3);
+            Assert.Equal(5, resAll);
+
         }
     }
 }
