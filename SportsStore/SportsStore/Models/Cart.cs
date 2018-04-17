69cad7df7f1bee4a2eac9ef9b76608bc666f9807
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-17 22:36:36 +0300

    Created => Cart.cs: 1. Cart class was created;

--- SportsStore/SportsStore/Models/Cart.cs
+++ SportsStore/SportsStore/Models/Cart.cs
@@ -7,5 +7,33 @@ namespace SportsStore.Models
 {
     public class Cart
     {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = this.lineCollection
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+
+            if(line == null)
+            {
+                this.lineCollection.Add(new CartLine{
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) => this.lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+
+        public virtual decimal ComputeTotalValue() => this.lineCollection.Sum(l => l.Product.Price * l.Quantity);
+
+        public virtual void Clear() => this.lineCollection.Clear();
+
+        public virtual IEnumerable<CartLine> Lines => lineCollection;
     }
 }
