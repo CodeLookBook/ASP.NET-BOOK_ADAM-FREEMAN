8637a4c073b61d755412e1327ae0b0412d45e33a
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-17 23:12:51 +0300

    Edited => CartTest.cs: 1. AddItem_CanAddQuantityForExistingLines() test method was created;

--- SportsStore/SportsStore.Tests/Models/CartTests.cs
+++ SportsStore/SportsStore.Tests/Models/CartTests.cs
@@ -28,5 +28,28 @@ namespace SportsStore.Tests.Models
             Assert.Equal(p1, results[0].Product);
             Assert.Equal(p2, results[1].Product);
         }
+
+        [Fact]
+        public void AddItem_CanAddQuantityForExistingLines()
+        {
+            // Arrange - create some test producrs
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            // Arrange - create a new cart
+            Cart target = new Cart();
+
+            // Act
+            target.AddItem(p1, 1 );          
+            target.AddItem(p2, 1 );
+            target.AddItem(p1, 10);
+
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray();
+
+            // Assert
+            Assert.Equal(2 , results.Length     );
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1 , results[1].Quantity);
+        }
     }
 }
