8678a417e140e31a5c784b1971fa9318001962b9
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-22 21:58:08 +0300

    Created => CartController.cs: 1. CartController class was created;

---
+++ SportsStore/SportsStore/Controllers/CartController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductRepository repository;
+
+        public CartController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public RedirectToActionResult AssToCart(int productId, string returnUrl)
+        {
+            Product product = this.repository
+                .Products
+                .FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                Cart cart = this.GetCart();
+
+                cart.AddItem(product, 1);
+
+                this.SaveCart(cart);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private void SaveCart(Cart cart)
+        {
+            HttpContext.Session.SetJson("Cart", cart);            
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+
+            return cart;
+        }
+    }
+}
