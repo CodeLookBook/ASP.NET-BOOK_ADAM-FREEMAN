b9cb2ec76ba3492c1aa34d2e2287b593efbf110c
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-04-22 21:19:50 +0300

    Edited => Startup.cs: 1. Session was enabled;

--- SportsStore/SportsStore/Startup.cs
+++ SportsStore/SportsStore/Startup.cs
@@ -28,6 +28,12 @@ namespace SportsStore
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(this.Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
+
+            // sets up the in-memory data store
+            services.AddMemoryCache();
+
+            // registers the services used to access session data
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +52,11 @@ namespace SportsStore
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+
+            // allows the session system to automatically associate requests 
+            // with sessions when they arrive from the client
+            app.UseSession();
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
